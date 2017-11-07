583a9a6756f773925540d42c05107873ee49e1e7
Raphael Peguet <11678530+raphpeguet@users.noreply.github.com>
2017-11-07 12:24:58 +0000

    Ajout des solutions pour chaque module

---
+++ SOLUTIONS/MODULE_1/2/visualizejs_main.js
@@ -0,0 +1,11 @@
+visualize({
+    auth: {
+        name: "jasperadmin",
+        password: "jasperadmin",
+        organization: "organization_1"
+    }
+}, function (v) {
+    /*ICI NOUS AJOUTERONS NOTRE CODE VISUALIZE>JS*/
+    alert('Visualize.js est prêt')
+ 
+}); 
---
+++ SOLUTIONS/MODULE_3/1/visualizejs_main.js
@@ -0,0 +1,62 @@
+visualize({
+    auth: {
+        name: "jasperadmin",
+        password: "jasperadmin",
+        organization: "organization_1"
+    }
+}, function (v) {
+	
+	/*-- MODULE_2_2 --*/
+	v("#div_chart_1").report({
+	    resource: "/public/Samples/Reports/02._Sales_Mix_by_Demographic_Report",
+	    scrollToTop:false,
+	    scale: "height",
+	    error: function (err) {
+	        alert(err.message);
+	    }
+	});
+	
+	/*-- MODULE_2_3 --*/
+	v("#div_chart_2").report({
+	    resource: "/public/Samples/Reports/States",
+	    scrollToTop:false,
+	    scale: "height",
+	    linkOptions: {
+	        events: {
+	            "mouseOut":function (ev, link) {},
+	            "mouseOver":function (ev, link) {},
+	            "select":function (ev, link) {},
+	            "click": function (ev, link) {             
+	                if (link.type == "ReportExecution") {
+						/*-- MODULE_2_4 --*/
+						//alert('Vous avez cliqué sur l\'État: '+ link.parameters.store_state);	
+					    v("#div_chart_3").report({
+					        resource: "/public/Samples/Reports/Cities",
+					        scrollToTop:false,
+					        scale: "height",
+					        params: {
+					            state: [link.parameters.store_state]
+					        }
+					    });
+					    //console.log(link.parameters.store_state);
+					}
+	            }
+	        }
+	    },
+	    error: function (err) {
+	        alert(err.message);
+	    }
+	});
+
+	/*-- MODULE_3_1 --*/
+	var drinkFoodReport = v.report({
+		resource: "/public/Samples/Reports/06g.ProfitDetailReport",
+		scrollToTop:false,
+		scale: "height",
+		container:"#div_report_1",
+		error: function (err) {
+			alert(err.message);
+		}
+	});
+ 
+});
